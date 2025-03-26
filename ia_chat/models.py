ff42b32a23c304f220924b456b4bdb78aac80145
Liddeep <liddeep@gmail.com>
2025-03-26 13:18:38 +0000

    update

--- ia_chat/models.py
+++ ia_chat/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Conversation(models.Model):
+    initial_prompt = models.TextField()
+    generated_prompt = models.TextField()
+    bot_response = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    # user = models.ManyToManyField("auth.User")
+
+    def __str__(self):
+        return f"Initial: {self.initial_prompt} | Response: {self.bot_response}"
