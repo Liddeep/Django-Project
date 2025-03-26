2b27500307ea1a1e23b7efbd5df81233ecf65665
Liddeep <liddeep@gmail.com>
2025-03-26 17:36:08 +0000

    update

--- gestion/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('perfil/', views.PerfilUsuario, name='perfil_usuario'),
-    path('ia/', views.ia_endpoint, name='ia_endpoint'),
-]
