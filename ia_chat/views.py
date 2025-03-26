b5154aee28dc370231a0bd504b689ce345ff3ae0
Liddeep <liddeep@gmail.com>
2025-03-26 17:46:55 +0000

    update

--- ia_chat/views.py
+++ ia_chat/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 import requests
 from .models import Conversation
-from rest_framework.decorators import APIView, authentication_classes, permission_classes
+from rest_framework.decorators import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
